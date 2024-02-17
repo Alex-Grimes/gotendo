592eeac649e0a7fcbad5f2b981814af1f1094729
alex <alexgrimes.se@gmail.com>
2024-02-17 15:28:52 -0500

    add functions for managing memory and test

--- m6502/memory.go
+++ m6502/memory.go
@@ -15,3 +15,36 @@ type BasicMemory struct {
 	DisableReads  bool
 	DisableWrites bool
 }
+
+func newBasicMemory(size uint32) *BasicMemory {
+	return &BasicMemory{
+		M: make([]uint8, size),
+	}
+}
+
+func (m *BasicMemory) Reset() {
+	for i := range m.M {
+		m.M[i] = 0xff
+	}
+}
+
+func (m *BasicMemory) Fetch(address uint16) (value uint8) {
+	if m.DisableReads {
+		value = 0xff
+	} else {
+		value = m.M[address]
+	}
+	return
+}
+
+func (m *BasicMemory) Store(address uint16, value uint8) (oldValue uint8) {
+	if m.DisableWrites {
+		oldValue = m.M[address]
+		m.M[address] = value
+	}
+	return
+}
+
+func SamePage(address1 uint16, address2 uint16) bool {
+	return (address1^address2)>>8 == 0
+}
